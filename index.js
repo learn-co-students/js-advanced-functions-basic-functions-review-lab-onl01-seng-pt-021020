55cd9f6337c267fffef84e367151334cc2ff2d65
TerryThreatt <terry.threatt@gmail.com>
2020-08-28 17:44:26 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,41 @@
 // Your code here
+
+// Function Declaration
+function saturdayFun(activity = "roller-skate") {
+    return `This Saturday, I want to ${activity}!`;
+}
+
+// Function Expression
+const mondayWork = function(activity = "go to the office") {
+    return `This Monday, I will ${activity}.`;
+}
+
+// Closure
+function wrapAdjective(flair = "*") {
+    return function inner(feeling = "special") {
+        return `You are ${flair}${feeling}${flair}!`;
+    }
+}
+
+// Object - first class functions
+const Calculator = {
+    add: function(a, b) {
+        return a + b;
+    },
+    subtract: function(a, b) {
+        return a - b;
+    },
+    multiply: function(a, b) {
+        return a * b;
+    },
+    divide: function(a, b) {
+        return a / b;
+    }
+}
+
+function actionApplyer(startInt, arr) {
+    for (let i = 0; i < arr.length; i++) {
+        startInt = arr[i](startInt)
+    }
+    return startInt;
+}
